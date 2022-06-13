b2bba50365bcaabb5b17acf32c98f826dbe3b6af
renxiaozhi <renxz@chinatelecom.cn>
2022-06-13 13:57:20 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sagerenn/pdfox
+
+go 1.17
+
+require github.com/pdfcpu/pdfcpu v0.3.12 // indirect
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
+
+func main()  {
+	pdfcpu.Object()
+}
